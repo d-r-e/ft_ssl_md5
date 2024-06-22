54139d51e29319fbeca43863ccc10484d6c04655
darodrig <d-r-e>
2024-06-22 14:07:02 +0200

    recode

--- Makefile
+++ Makefile
@@ -1,50 +1,28 @@
-NAME= ft_ssl
+NAME=ft_ssl
+SRC = ./src/main.c 
 
-SRC = src/main.c src/md5.c src/byte.c
+CC=gcc
+CFLAGS=-Wall -g -O2 -Wextra -Werror -Wformat-security
+
+INC=-I./inc
+
+all: $(NAME)
+
 OBJ = $(SRC:.c=.o)
-FLAGS= -Wall -Wextra -Werror -Wformat-security
-LIBFT=libft/libft.a
-INC=inc/$(NAME).h
 
-$(NAME): $(LIBFT) $(OBJ) 
-	gcc $(FLAGS) $(OBJ) -I libft -L libft -lft -o $(NAME)
+$(NAME): $(OBJ)
+	$(CC) $(CFLAGS) $(INC) -o $(NAME) $(OBJ)
 
-$(LIBFT):
-	$(MAKE) -C libft
-
-%.o: %.c $(INC)
-	gcc $(FLAGS) -c -o $@ $<
+%.o: %.c
+	$(CC) $(CFLAGS) $(INC) -c -o $@ $<
 
 clean:
-	$(MAKE) clean -C libft
 	rm -f $(OBJ)
 
 fclean: clean
-	$(MAKE) fclean -C libft
 	rm -f $(NAME)
 
 re: fclean all
 
-all: $(NAME)
 
-x: $(NAME)
-	./$(NAME)
-n:	$(NAME)
-	echo -n "" | ./$(NAME) md5
-
-long: $(NAME)
-	python3 -c "print('B'*(0), end='')" | ./$(NAME) md5
-	@echo
-	@python3 -c "print('B'*(55+(64 * 7)), end='')" | ./$(NAME) md5
-
-commit: all long fclean 
-	@git add $(SRC) Makefile $(INC) .gitignore 
-	@git commit -am "auto commit by darodrig `date +%d-%m-%Y` from `uname -n | cut -d. -f1`"
-
-push: commit
-	@git push origin main
-
-norm:
-	@norminette -v
-	norminette $(SRC) $(INC)
-.PHONY: re clean fclean all
+.PHONY: clean re all make fclean
--- inc/ft_ssl.h
+++ inc/ft_ssl.h
@@ -1,82 +1,13 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_ssl.h                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: darodrig <darodrig@42madrid.com>           +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/16 10:20:24 by darodrig          #+#    #+#             */
-/*   Updated: 2021/08/28 20:10:18 by darodrig         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef FT_SSL_H
-#define FT_SSL_H
+# define FT_SSL_H
 
-#include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <fcntl.h>
-#include <math.h>
 #include <stdint.h>
-#include "../libft/libft.h"
 
-typedef union s_word
-{
-	uint32_t i;
-	struct
-	{
-		uint8_t b0 : 8;
-		uint8_t b1 : 8;
-		uint8_t b2 : 8;
-		uint8_t b3 : 8;
-	} bit;
-} t_word;
 
-typedef union s_block
-{
-	char str[64];
-	t_word words[16];
-} t_block;
+typedef struct md5_state {
+    uint32_t buffer[64];
+} t_md5_block;
 
-typedef int (*command)(const char *s);
-
-typedef struct s_ssl
-{
-	int p;
-	int r;
-	int s;
-	int q;
-	int md5;
-	int sha256;
-	char *str;
-	char *msg;
-	char *input;
-	char *file;
-	uint64_t len;
-} t_ssl;
-
-typedef struct s_md5
-{
-	t_word a;
-	t_word b;
-	t_word c;
-	t_word d;
-	char *msg;
-	t_block *blocks;
-	uint32_t nblocks;
-	uint64_t len;
-	uint64_t flen;
-} t_md5;
-
-t_ssl g_ssl;
-
-int ft_append(char **buff, char c);
-int ft_append_at(char **buff, char c, unsigned int pos);
-int ft_md5(t_ssl ssl);
-t_word init_word(uint8_t a, uint8_t b, uint8_t c, uint8_t d);
-t_word init_word_int(int n);
-void print_word(t_word w);
-void print_block(t_block b);
-void print_md5_hash(t_md5 md5);
 #endif
